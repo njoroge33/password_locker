27177a5f56f03494024b56df3ddc00e2db782ab7
njoroge33 <johngichuhi769@gmail.com>
2019-11-09 13:28:56 +0300

    create functions for run.py file

--- run.py
+++ run.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3.6
 from locker import *
 
+def create_account(user_name, passsword):
+    '''
+    Function to create a new user account
+    '''
 
+    new_user = User(user_name, passsword)
+    return new_user
+
+def save_account(user):
+    '''
+    Function to save user account
+    '''
+    User.create_account()
+
+def login(user_name, passsword):
+    '''
+    Function that check if a user exists with that number and return a Boolean
+    '''
+    return User.user_exist(user_name, passsword)
+
+def create_credential(account_name, passsword):
+
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credential(account_name, passsword)
+    return new_credential
+
+def save_credential(credential):
+    '''
+    Function to save a credentials
+    '''
+    return Credential.save_credential()
+
+def find_credential(account_name):
+    return Credential.find_by_account_name(account_name)
+
+def view_credential():
+    return Credential.view_all_credentials()
+
+def delete_credential():
+    return Credential.delete_credential()
