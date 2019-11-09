1bb7ea0154fec498d2ae2fdf61c7e66f3e2a7ad7
njoroge33 <johngichuhi769@gmail.com>
2019-11-09 10:53:02 +0300

    create a function to create a class

--- locker.py
+++ locker.py
@@ -1,7 +1,15 @@
 class User:
+    users = []
+
     def __init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
+
+    def create_account(self):
+        User.users.append(self)
+
+   
+
 
 class Credential:
     pass
